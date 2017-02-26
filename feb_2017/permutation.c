7a70ce304bbd4fa4a6c8797fafd869f969c4c895
Mankamana <mankamana_mishra@yahoo.com>
2017-02-25 22:52:56 -0800

    committing permutation

--- feb_2017/permutation.c
+++ feb_2017/permutation.c
@@ -1,2 +1,19 @@
+/*                                                                              
+ *------------------------------------------------------------------            
+ * permutation.c              
+ *                                                                              
+ * Jan 2017, Mankamana Mishra 
+ *                                                                              
+ * Copyright (c) 2017 by Mankamana Mishra.                               
+ * All rights reserved.                                                         
+ *------------------------------------------------------------------            
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
 
 
+int main ()
+{
+    char str [5] = "1234"
+}
